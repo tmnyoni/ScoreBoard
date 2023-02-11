5946d552e22a39549d28841d546b7aeea0c29a8f
Tawanda M <tmnyoni@outlook.com>
2023-02-11 08:50:59 +0200

    added navigation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomePage from './pages/Home';
+import { HomeScreen, GameScreen } from './screens';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <HomePage />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Game' component={GameScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
