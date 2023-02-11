04dcdf469a1c0e81f9e1a1744a1e3df7d44ca224
Tawanda M <tmnyoni@outlook.com>
2023-02-11 11:36:56 +0200

    added in-game elements

--- screens/Game.tsx
+++ screens/Game.tsx
@@ -1,9 +1,64 @@
-import { SafeAreaView, Text } from "react-native";
+import { useEffect, useState } from "react";
+import {
+    SafeAreaView, Text,
+    StyleSheet,
+    View,
+    TextInput,
+    Button
+} from "react-native";
 
-export default function GameScreen() {
+import type { Player } from "../components/Player";
+
+export default function GameScreen({ navigation, route }) {
+
+    const players: Player[] = route.params.players;
+    const [round, setRound] = useState<number>(1);
+
+    function nextRound() {
+        setRound(prevRound => prevRound + 1)
+    }
+
+    function endPlayerTurn() {
+        // set the player score.
+        // set next player
+        nextRound();
+    }
+
+    function endGame() {
+        // end the game logic.
+    }
+
+    useEffect(() => {
+        console.log(players)
+    }, [players]);
+
     return (
-        <SafeAreaView>
-            <Text> Player Turn </Text>
+        <SafeAreaView style={styles.container}>
+            <View>
+                <Text style={styles.title}> Round {round} </Text>
+                <Text style={{ borderWidth: 1, textAlign: "center" }}> {players[0].name} Turn </Text>
+                <TextInput placeholder="Enter score" />
+                <Button title="End turn" onPress={endPlayerTurn} />
+                <Text onPress={endGame}>
+                    End Game
+                </Text>
+            </View>
         </SafeAreaView>
     )
 }
+
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 20,
+        textAlign: 'center',
+        fontWeight: "500",
+        marginTop: 80,
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        padding: 24,
+    },
+});
